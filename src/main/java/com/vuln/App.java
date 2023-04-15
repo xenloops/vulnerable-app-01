51705234151d9ce1bef5fd99e617d81fc57546de
Nathan Larson <nalarson@gmail.com>
2023-04-15 16:48:44 -0500

    Remove old code

--- src/main/java/com/vuln/App.java
+++ src/main/java/com/vuln/App.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 public class App {
-//    private static final Logger logger = Logger.getLogger(App.class);
+
     private static final Logger logger = LogManager.getLogger(App.class);
     public static void main(String[] args) {
         logger.debug("This is a debug message.");
