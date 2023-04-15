2c697a2b905f53aa91aeccde513fff1e47399e51
xenloops <57366429+xenloops@users.noreply.github.com>
2023-04-15 16:21:06 -0500

    Add files via upload

---
+++ src/main/java/com/vuln/App.java
@@ -0,0 +1,16 @@
+package com.vuln;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class App {
+//    private static final Logger logger = Logger.getLogger(App.class);
+    private static final Logger logger = LogManager.getLogger(App.class);
+    public static void main(String[] args) {
+        logger.debug("This is a debug message.");
+        logger.info("This is an info message.");
+        logger.warn("This is a warning message.");
+        logger.error("This is an error message.");
+        logger.fatal("This is a fatal message.");
+    }
+}
